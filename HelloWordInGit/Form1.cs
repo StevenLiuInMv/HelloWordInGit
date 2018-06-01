7722bc68078515a1a56c1f7c74bef96af403c7bb
stevenliuinmv <stevenliu@machvision.com.tw>
2018-06-01 09:24:44 +0800

    test1

--- HelloWordInGit/Form1.cs
+++ HelloWordInGit/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWordInGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
     }
 }
